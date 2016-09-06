d593ef3674bd8a407c2025e39ea75b8b8c3fe736
Lloyd <lloyd.niebel@googlemail.com>
2016-09-06 17:18:19 +0200

    Implemented first Prototype of Entity Framework
    EF creates an entry in Database and write to it - has to be remodeled
    etc.

--- KanBan-App/Backend/Controllers/ValuesController.cs
+++ KanBan-App/Backend/Controllers/ValuesController.cs
@@ -13,14 +13,14 @@ namespace Backend.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3"};
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return id.ToString();
         }
 
         // POST api/values
