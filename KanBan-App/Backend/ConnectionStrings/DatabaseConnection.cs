3cb06f0fa1a5b645eb4373511a1c0809c1529299
Kulu-M <lloyd.niebel@googlemail.com>
2016-09-13 21:37:05 +0200

    Moved DB Con String to Res

---
+++ KanBan-App/Backend/ConnectionStrings/DatabaseConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.ConnectionStrings
+{
+    public static class DatabaseConnection
+    {
+        // Insert DB Con String here
+        public static string DatabaseConnectionString = ""; 
+    }
+}
