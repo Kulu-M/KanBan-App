b52fd7394243b76a30fce102239228d998ccdb78
Lloyd <lloyd.niebel@googlemail.com>
2016-09-17 18:18:59 +0200

    This was not needed

--- KanBan-App/Backend/Program.cs
+++ KanBan-App/Backend/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Backend.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 
