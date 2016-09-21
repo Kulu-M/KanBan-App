1431c587a347b600558a9e51a9db68f34994a04d
Lloyd <lloyd.niebel@googlemail.com>
2016-09-21 16:18:42 +0200

    Frontend Model added

---
+++ KanBan-App/Frontend/Model/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend
+{
+    public class Note
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AppointedPerson { get; set; }
+        public long? BoardId { get; set; }
+    }
+}
---
+++ KanBan-App/Frontend/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend
+{
+    public class User
+    {
+        public string EMail { get; set; }
+        public string Password { get; set; }
+        public string VerificationKey { get; set; }
+    }
+}
