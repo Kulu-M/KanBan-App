08f95b1b2fc683c72ac7171137b9b1aa6799179e
Lloyd <lloyd.niebel@googlemail.com>
2016-09-22 16:39:45 +0200

    New Frontend Requests

---
+++ KanBan-App/Frontend/Classes/BoardRequests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Frontend
+{
+    public class BoardRequests
+    {
+        public async static Task<string> createNewNote(string email, string password)
+        {
+            using (var client = new HttpClient())
+            {
+                var adress = "http://localhost:5000/api/board/note/create";
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var note = new Note {Description = "FinalTestNote", Name = "SuperNote", BoardId = 1};
+                var json = JsonConvert.SerializeObject(note);
+
+                HttpResponseMessage response =
+                    await client.PostAsync(adress, new StringContent(json, Encoding.UTF8, "application/json"));
+
+                return response.ReasonPhrase;
+            }
+        }
+    }
+}
---
+++ KanBan-App/Frontend/Classes/UserRequests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Frontend
+{
+    public class UserRequests
+    {
+        public async static Task<string> registerNewUser(string email, string password)
+        {
+            using (var client = new HttpClient())
+            {
+                var adress = "http://localhost:5000/api/user/register/";
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var User = new User {EMail = email, Password = password};
+                var jsonUser = JsonConvert.SerializeObject(User);
+
+                HttpResponseMessage response =
+                    await client.PostAsync(adress, new StringContent(jsonUser, Encoding.UTF8, "application/json"));
+
+                return response.ReasonPhrase;
+            }
+        }
+    }
+}
